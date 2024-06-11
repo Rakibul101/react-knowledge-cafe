83871fed32a65f2076c52c0447592b54b1dfe99e
Rakibul101 <rakibulislam.dev07@gmail.com>
2024-06-11 08:09:20 +0600

    load data in website

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,11 @@
+import { CiBookmark } from "react-icons/ci";
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+const Blog = ({blog, handleAddToBookMark}) => {
     const {title, cover, author, posted_date, author_img, reading_time,hashtags} = blog;
     return (
-        <div>
-            <img src={cover} alt="" />
-            <div className='flex justify-between'>
+        <div className='mb-10'>
+            <img className='w-full rounded-lg mb-6' src={cover} alt="" />
+            <div className='flex justify-between mb-6'>
                 <div className='flex gap-6'>
                     <img className='w-14' src={author_img} alt="" />
                     <div className=''>
@@ -12,12 +13,13 @@ const Blog = ({blog}) => {
                         <p>{posted_date}</p>
                     </div>
                 </div>
-                <div>
+                <div className='flex items-center'>
                     <span>{reading_time} min read</span>
+                    <button onClick={handleAddToBookMark} className="ml-2 text-xl"><CiBookmark /></button>
                 </div>
             </div>
-            <h2 className='text-4xl'>{title}</h2>
-            <p>
+            <h2 className='text-4xl mb-4'>{title}</h2>
+            <p className='mb-6'>
                 {
                     hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
                 }
